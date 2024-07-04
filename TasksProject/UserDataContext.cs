c40510f172d238ce55ee1ba848534106c96da625
NaifAliYahi <167720139+NaifAliYahi@users.noreply.github.com>
2024-07-03 16:29:47 -0900

    all

---
+++ TasksProject/UserDataContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TasksProject
+{
+    public class UserDataContext : DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        optionsBuilder.UseSqlServer("Data Source=localhost;Database=UserDemoDB;Integrated Security=True;Encrypt=False;Trust Server Certificate=False");
+
+
+        }  
+        public DbSet<User> Users { get; set; }
+        public DbSet<AddUsers> AddUsers { get; set; }
+
+
+        //   protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //     modelBuilder.Entity<person>()
+        //.ToTable("persons");
+
+        //     modelBuilder.Entity<PersonWithPrice>()
+        //         .ToTable("persons")
+        //         .HasBaseType<person>();
+        // } 
+
+    }
+
+}
